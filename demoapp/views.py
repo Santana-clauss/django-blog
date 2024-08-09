fb993c26f33f0b8efd1f933e50ae6f06d499539b
santana clauss <jepchumbasantana@gmail.com>
2024-08-09 16:06:33 +0300

    updated views

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def homepage(request):
+   content="<html><body><h1>My blog</h1></body></html>" 
+   return HttpResponse(content)
